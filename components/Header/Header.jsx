868c7b6c35ad413b0e62d7451a48b83ce9385b5b
Artyomsheq <artimushka@gmail.com>
2023-06-22 15:32:38 +0600

    rework

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -42,68 +42,82 @@ const Header = () => {
   };
 
   return (
-    <div className={s.header}>
-      <div className={s.header_top_row}>
-        <div className={s.header_top_row_logo}>
-          <h3>Logo</h3>
+    <div className="container">
+      <div className={s.header}>
+        <div className={s.header_top_row}>
+          <div className={s.header_top_row_logo}>
+            <h3>Logo</h3>
+          </div>
+          <div className={s.header_top_row_contacts}>
+            <div className={s.top_row_contacts_inp}>
+              <Image src="/loop.png" alt="loop" width={15} height={15} />
+              <input type="text" placeholder="Search here..." />
+            </div>
+            <div className={s.top_row_contacts_mail}>
+              <h3>@</h3>
+              <a href="#">email@mail.com</a>
+            </div>
+            <div className={s.top_row_contacts_number}>
+              <Image src="/loop.png" alt="loop" width={15} height={15} />
+              <a href="#">(000) 888-88</a>
+            </div>
+            <button onClick={toggleMenu}>
+              <div
+                className={`${s.header_burger_line} ${
+                  menuOpen ? s.burger_line_rotate : ""
+                }`}
+              ></div>
+            </button>
+          </div>
         </div>
-        <div className={s.header_top_row_contacts}>
-          <div className={s.top_row_contacts_inp}>
-            <Image src="/loop.png" alt="loop" width={15} height={15} />
-            <input type="text" placeholder="Search here..." />
-          </div>
-          <div className={s.top_row_contacts_mail}>
-            <h3>@</h3>
-            <a href="#">email@mail.com</a>
-          </div>
-          <div className={s.top_row_contacts_number}>
-            <Image src="/loop.png" alt="loop" width={15} height={15} />
-            <a href="#">(000) 888-88</a>
-          </div>
-          <button onClick={toggleMenu}>
-            <div className={`${s.header_burger_line} ${menuOpen ? s.burger_line_rotate : ""}`}></div>
-          </button>
-        </div>
+        <AnimatePresence>
+          {menuOpen && (
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={{
+                hidden: { opacity: 0 },
+                visible: { opacity: 1 },
+              }}
+              className={`${s.header_bottom_row} ${menuOpen ? s.show : ""}`}
+            >
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                custom={1}
+                variants={textAnimation}
+                className={s.header_bottom_row_links}
+              >
+                <Link href="#">Home</Link>
+                <Link href="#">Products</Link>
+                <Link href="#">Pages</Link>
+                <Link href="#">Blog</Link>
+                <Link href="#">Contact us</Link>
+              </motion.div>
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                custom={2}
+                variants={textAnimation}
+                className={s.header_bottom_row_btn}
+              >
+                <Image src="/globus.png" alt="globus" width={20} height={20} />
+                <h3>English</h3>
+                <button>
+                  Sign up
+                  <Image
+                    src="/btnArrow.png"
+                    width={5}
+                    height={8}
+                    alt="btnArrow"
+                  />
+                </button>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      <AnimatePresence>
-        {menuOpen && (
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 1 },
-            }}
-            className={`${s.header_bottom_row} ${menuOpen ? s.show : ""}`}
-          >
-            <motion.div
-              initial="hidden"
-              whileInView="visible"
-              custom={1}
-              variants={textAnimation}
-              className={s.header_bottom_row_links}
-            >
-              <Link href="#">Home</Link>
-              <Link href="#">Products</Link>
-              <Link href="#">Pages</Link>
-              <Link href="#">Blog</Link>
-              <Link href="#">Contact us</Link>
-            </motion.div>
-            <motion.div
-              initial="hidden"
-              whileInView="visible"
-              custom={2}
-              variants={textAnimation}
-              className={s.header_bottom_row_btn}
-            >
-              <Image src="/globus.png" alt="globus" width={20} height={20} />
-              <h3>English</h3>
-              <button>Sign up</button>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
     </div>
   );
 };
