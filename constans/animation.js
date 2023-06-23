0b496142a62b5129226bb2594dac4636ada21882
Artyomsheq <artimushka@gmail.com>
2023-06-23 13:59:22 +0600

    rework

--- constans/animation.js
+++ constans/animation.js
@@ -30,3 +30,31 @@ export const visibleTopAnimation = (customTop) => ({
     opacity: 1,
     transition: { delay: customTop * 0.2}
 })
+
+export const textAnimation = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    visible: custom => ({
+      x: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.1}
+    }),
+  };
+  
+  export const textAnimationtop = {
+    hidden: {
+      y: -100,
+      opacity: 0,
+    },
+    hidden: {
+      y: -100,
+      opacity: 0,
+    },
+    visible: custom => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * .4}
+    }),
+  };
